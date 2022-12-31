c1223ac78555506ca6565101e6f1994b61e8134d
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2022-12-30 22:31:22 -0800

    feat: create `EmployeeController`
    `EmployeeController` is responsible for handling the requests coming
    from clients. Specifically, it injects `EmployeeService` as a dependency
    to perform HTTP methods (POST, PUT, GET, DELETE).
    
    Create HRIS.postman_collection.json file for Postman testing.
    
    Update README.md document with a new field `3. APIs Testing`.
    
    Issue #9

---
+++ src/main/java/com/retrotechie/rthris/employee/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.retrotechie.rthris.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // Combination of @Controller & @ResponseBody
+@RequestMapping("/api") // Define a base URL for all REST APIs created in this controller
+public class EmployeeController {
+    @Autowired  // Inject EmployeeService as a dependency
+    private EmployeeService employeeService;
+
+    // CREATE
+    // `value` is the endpoint -> full URL: `/api/employee`
+    // `method` is the HTTP methods (POST, PUT, GET, DELETE)
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    // GET
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    public List<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    // UPDATE
+    @RequestMapping(value = "/employees/{employeeId}", method = RequestMethod.PUT)
+    // `@PathVariable` means the API endpoint has a path parameter involved
+    // Below code will map `employeeId` value from the endpoint to `id`
+    public Employee updateEmployee(@PathVariable(value = "employeeId") Long id,
+                                   @RequestBody Employee employeeDetails) {
+        return employeeService.updateEmployee(id, employeeDetails);
+    }
+
+    // DELETE
+    @RequestMapping(value = "/employees/{employeeId}", method = RequestMethod.DELETE)
+    public void deleteEmployee(@PathVariable(value = "employeeId") Long id) {
+        employeeService.deleteEmployee(id);
+    }
+}
