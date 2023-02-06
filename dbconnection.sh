bfb75a36598199c87d70ba63bdd2a5362a005fc0
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-05 18:11:49 -0800

    ci(Jenkins): fix updating database credentials
    Issue #45

--- dbconnection.sh
+++ dbconnection.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sed -i 's/_database_/$1/' src/main/resources/application.properties
-sed -i 's/_username_/$2/' src/main/resources/application.properties
-sed -i 's/_password_/$3/' src/main/resources/application.properties
+cp ./src/main/resources/application.properties ./src/main/resources/application.properties.bak
+sed 's/_database_/$1/g; s/_username_/$2/g; s/_password_/$3/g' ./src/main/resources/application.properties.bak > ./src/main/resources/application.properties
