3e2ce131966a5972041682b9dcf9ca80cd5a2f71
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-05 17:44:49 -0800

    ci(Jenkins): fix shell syntax in dbconnection.sh
    Issue #45

--- dbconnection.sh
+++ dbconnection.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i 's/_database_/$1/' ./src/main/resources/application.properties
-sed -i 's/_username_/$2/' ./src/main/resources/application.properties
-sed -i 's/_password_/$3/' ./src/main/resources/application.properties
+sed -i 's/_database_/$1/' src/main/resources/application.properties
+sed -i 's/_username_/$2/' src/main/resources/application.properties
+sed -i 's/_password_/$3/' src/main/resources/application.properties
